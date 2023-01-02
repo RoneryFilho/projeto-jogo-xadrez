5216202de6b967da9ff8e3a3e361b5095b9c3e2b
Ronery Filho <roneryrffilho@gmail.com>
2023-01-02 14:37:43 -0300

    Implementing possible moves of a piece

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,15 +1,34 @@
 package boardgame;
 
-public class Piece {
+public abstract class Piece {
 	protected Position position;
 	private Board board;
-	
+
 	public Piece(Board board) {
 		this.board = board;
 		position = null;
 	}
-	
+
 	protected Board getBoard() {
 		return board;
+	}
+	//mostra os movimentos possiveis da peça
+	public abstract boolean[][] possibleMoves();
+	
+	//mostra no grid as possiveis casas que a peça pode ir
+	public boolean possibleMove(Position position) {
+		return possibleMoves()[position.getRow()][position.getCollumns()];
+	}
+	
+	public boolean isThereAnyPossibleMove() {
+		boolean[][] mat = possibleMoves();
+		for(int i=0;i<mat.length;i++) {
+			for(int j=0;j<mat.length;j++) {
+				if(mat[i][j]) {
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 }
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -7,12 +7,7 @@ import chess.pieces.King;
 import chess.pieces.Rook;
 
 public class ChessMatch {
-	private int turn;
-	private Color currentPlayer;
-	private boolean check;
-	private boolean checkMate;
-	private ChessPiece enPassantVulnearble;
-	private ChessPiece promoted;
+	
 	private Board board;
 
 	public ChessMatch() {
@@ -48,6 +43,9 @@ public class ChessMatch {
 	private void validateSourcePosition(Position position) {
 		if(!board.thereIsAPiece(position)) {
 			throw new ChessException("There is no piece in source position");
+		}
+		if(!board.piece(position).isThereAnyPossibleMove()) {
+			throw new ChessException("There are no moves for the chosen piece");
 		}
 	}
 	
