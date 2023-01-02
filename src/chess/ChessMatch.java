2c7e9cf779ccd2f8635da646f4343b0226fda7c6
Ronery Filho <roneryrffilho@gmail.com>
2023-01-02 11:16:26 -0300

    Improvements in board printing

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,7 +1,6 @@
 package chess;
 
 import boardgame.Board;
-import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
 
@@ -16,7 +15,7 @@ public class ChessMatch {
 
 	public ChessMatch() {
 		board = new Board(8, 8);
-		initialPosition();
+		initialSetup();
 	}
 
 	public ChessPiece[][] getPieces() {
@@ -33,12 +32,19 @@ public class ChessMatch {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
 	}
 	
-	private void initialPosition() {
-		placeNewPiece('h', 8, new Rook(board, Color.WHITE));
-		placeNewPiece('a', 8, new Rook(board, Color.WHITE));
-		placeNewPiece('e', 8, new King(board, Color.WHITE));
-		placeNewPiece('h', 1, new Rook(board, Color.BLACK));
-		placeNewPiece('a', 1, new Rook(board, Color.BLACK));
-		placeNewPiece('e', 1, new King(board, Color.BLACK));
+	private void initialSetup() {
+		placeNewPiece('c', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('c', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 1, new King(board, Color.WHITE));
+
+        placeNewPiece('c', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('c', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 8, new King(board, Color.BLACK));
 	}
 }
