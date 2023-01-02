4cf91d8a01d4824804f992f29d0c981b2ce1d4e6
Ronery Filho <roneryrffilho@gmail.com>
2023-01-02 11:00:43 -0300

    Added ChessException and ChessPosition

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -29,12 +29,16 @@ public class ChessMatch {
 		return mat;
 	}
 	
+	private void placeNewPiece(char column, int row, ChessPiece piece) {
+		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+	}
+	
 	private void initialPosition() {
-		board.placePiece(new Rook(board, Color.WHITE), new Position(0,7));
-		board.placePiece(new Rook(board, Color.WHITE), new Position(0,0));
-		board.placePiece(new King(board, Color.WHITE), new Position(0,3));
-		board.placePiece(new Rook(board, Color.BLACK), new Position(7,7));
-		board.placePiece(new Rook(board, Color.BLACK), new Position(7,0));
-		board.placePiece(new King(board, Color.BLACK), new Position(7,3));
+		placeNewPiece('h', 8, new Rook(board, Color.WHITE));
+		placeNewPiece('a', 8, new Rook(board, Color.WHITE));
+		placeNewPiece('e', 8, new King(board, Color.WHITE));
+		placeNewPiece('h', 1, new Rook(board, Color.BLACK));
+		placeNewPiece('a', 1, new Rook(board, Color.BLACK));
+		placeNewPiece('e', 1, new King(board, Color.BLACK));
 	}
 }
